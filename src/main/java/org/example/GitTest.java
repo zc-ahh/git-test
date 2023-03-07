59e37956f9d01230f97e207b733826b69879030c
zc <166233495@qq.com>
2023-03-07 16:06:56 +0800

    push commit1

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello test3");
         System.out.println("hello master");
         System.out.println("hello hot-fix");
+        System.out.println("push test");
     }
 }
