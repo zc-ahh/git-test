7f87dfae92f235b8aa7c6e7576af3c92f22e8e0c
zc <166233495@qq.com>
2023-03-07 15:43:42 +0800

    second commit

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -3,5 +3,6 @@ package org.example;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello test");
+        System.out.println("hello test1");
     }
 }
