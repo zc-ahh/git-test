473bb1b04adc638cec8591510ce872ae28ba44c3
zc-ahh <75969977+zc-ahh@users.noreply.github.com>
2023-03-07 16:12:24 +0800

    Update GitTest.java

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("push test");
         System.out.println("push test2");
+        System.out.println("push test3");
     }
 }
